8b7dd6ea8bde0ef30c43141055fc3a9bb02855c0
Andrzej <49211041+AndrzejKuster@users.noreply.github.com>
2023-10-08 10:32:11 +0200

    frontend init

---
+++ src/components/AddClientForm.js
@@ -0,0 +1,18 @@
+const AddClientForm = () => {
+    return (
+        <form action="#">
+            <div className="wrapper">
+                <label htmlFor="name"></label>
+            </div>
+            <div className="wrapper">
+                <label htmlFor="address"></label>
+            </div>
+            <div className="wrapper">
+                <label htmlFor="nip"></label>
+            </div>
+            <div className="wrapper">
+                <button type="submit">Zapisz firmę</button>
+            </div>
+        </form>
+    )
+}
